7f0a09b2d012d3f8642f4766133ec8a864d83dfc
Brad Seftel <177548123+bradseftel@users.noreply.github.com>
2024-09-01 12:50:30 -0230

    Implemented Routing

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const { log } = require('console');
 const fs = require('fs');
 const http = require('http');
+const url = require('url')
 /////////////////////////////
 // FILES
 
@@ -14,26 +14,50 @@ const http = require('http');
 // console.log('File Written');
 
 //non-blocking
-fs.readFile('./txt/start.txt','utf-8', (err, data1)=>{
-    if (err) return console.log("ERROR 🔥");
-    fs.readFile(`./txt/${data1}.txt`,'utf-8', (err, data2)=>{ 
-        console.log(data2);
+// fs.readFile('./txt/start.txt','utf-8', (err, data1)=>{
+//     if (err) return console.log("ERROR 🔥");
+//     fs.readFile(`./txt/${data1}.txt`,'utf-8', (err, data2)=>{ 
+//         console.log(data2);
 
-        fs.readFile(`./txt/append.txt`,'utf-8', (err, data3)=>{ 
-            console.log(data3);       
-            fs.writeFile('./txt/final.txt',`${data2}\n${data3}`, 'utf-8', err =>{
-                console.log("Your file has been written");
-            })
-        });
-    
-    });
+//         fs.readFile(`./txt/append.txt`,'utf-8', (err, data3)=>{ 
+//             console.log(data3);       
+//             fs.writeFile('./txt/final.txt',`${data2}\n${data3}`, 'utf-8', err =>{
+//                 console.log("Your file has been written");
+//             })
+//         });    
+//     });
+// });
 
-});
-
-console.log('Will Read FIle!');
+// console.log('Will Read FIle!');
 
 
 /////////////////////////////
 // SERVER
+const server = http.createServer((req, res)=>{
+    const pathName = req.url;
+    fs.readFileSync(`${__dirname}/dev-data/data.json`,'utf-8',(err, data)=>{            
+        const productData = JSON.parse(data);
+    });  
 
+    if(pathName === '/' || pathName === '/overview'){
+        res.end('Welcome to the overview.');
+    }else if(pathName === '/productname'){
+        res.end('Welcome to the products.');
+    }else if(pathName === '/api'){
+        fs.readFile(`${__dirname}/dev-data/data.json`,'utf-8',(err, data)=>{
+            
+            const productData = JSON.parse(data);
+            res.writeHead(200, {'content-type':'application/json'});
+            res.end(data);
+        });        
+    }else{
+        res.writeHead(404, {'Content-Type': 'text/html'});
+        res.end('<H1>Page not found.</H1>')
+    }
 
+    
+});
+
+server.listen(8000, '127.0.0.1', ()=>{
+    console.log('Listening on port 8000')
+});
