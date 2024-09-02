5d2f72d4116609ee8d97a697554f171b6c0eda6c
Brad Seftel <177548123+bradseftel@users.noreply.github.com>
2024-09-02 12:38:45 -0230

    2 of 3 bug fixes - lsat one is not organic logic

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const http = require('http');
 const url = require('url')
+
 /////////////////////////////
 // FILES
 
@@ -34,24 +35,57 @@ const url = require('url')
 /////////////////////////////
 // SERVER
 
-const data = fs.readFileSync(`${__dirname}/dev-data/data.json`,'utf-8');
+const replaceTemplate = (temp, product) =>{
+    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+    output = output.replace(/{%IMAGE%}/g, product.image);
+    output = output.replace(/{%PRICE%}/g, product.price);
+    output = output.replace(/{%FROM%}/g, product.from);
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+    output = output.replace(/{%QUANTITY%}/g, product.quantity);
+    output = output.replace(/{%DESCRIPTION%}/g, product.description);
+    output = output.replace(/{%ID%}/g, product.id);
+
+    if(!product.organic) output = output.replace(/{%NOTORGANIC%}/g, 'not organic');
+    //console.log(product.organic);
+    return output;
+    
+} 
+
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`,'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`,'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`,'utf-8');
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`);
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res)=>{
+    //OVERVIEW
+
     const pathName = req.url;
     if(pathName === '/' || pathName === '/overview'){
-        res.end('Welcome to the overview.');
+        res.writeHead(200, {'content-type':'text/html'});        
+        const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
+        const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+        res.end(output);
+        
+
+    //PRODUCTS
     }else if(pathName === '/productname'){
+
         res.end('Welcome to the products.');
+
+    //API
     }else if(pathName === '/api'){    
         res.writeHead(200, {'content-type':'application/json'});
         res.end(data);
+        
+    //NOT FOUND
     }else{
         res.writeHead(404, {'Content-Type': 'text/html'});
         res.end('<H1>Page not found.</H1>')
     }
 });
 
-server.listen(8000, '127.0.0.1', ()=>{
+    server.listen(8000, '127.0.0.1', ()=>{
     console.log('Listening on port 8000')
 });
