56584d88c5c1ea780d8dfa89c1695a967993f137
Brad Seftel <177548123+bradseftel@users.noreply.github.com>
2024-09-03 12:02:52 -0230

    Fixed all bugs! Lastly no, implementing links

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const http = require('http');
 const url = require('url')
+const replaceTemplate = require('./modules/replaceTemplate');
 
 /////////////////////////////
 // FILES
@@ -34,23 +35,6 @@ const url = require('url')
 
 /////////////////////////////
 // SERVER
-
-const replaceTemplate = (temp, product) =>{
-    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-    output = output.replace(/{%IMAGE%}/g, product.image);
-    output = output.replace(/{%PRICE%}/g, product.price);
-    output = output.replace(/{%FROM%}/g, product.from);
-    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-    output = output.replace(/{%QUANTITY%}/g, product.quantity);
-    output = output.replace(/{%DESCRIPTION%}/g, product.description);
-    output = output.replace(/{%ID%}/g, product.id);
-
-    if(!product.organic) output = output.replace(/{%NOTORGANIC%}/g, 'not organic');
-    //console.log(product.organic);
-    return output;
-    
-} 
-
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`,'utf-8');
 const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`,'utf-8');
 const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`,'utf-8');
@@ -66,12 +50,10 @@ const server = http.createServer((req, res)=>{
         res.writeHead(200, {'content-type':'text/html'});        
         const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
         const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
-        res.end(output);
-        
+        res.end(output);        
 
     //PRODUCTS
     }else if(pathName === '/productname'){
-
         res.end('Welcome to the products.');
 
     //API
